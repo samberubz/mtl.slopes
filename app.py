b2db984b693f2cb3a9daf573f36d91a7afe3a3dc
samberubz <samuel.berube.1@ens.etsmtl.ca>
2024-08-17 12:14:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ image2_url = 'https://raw.githubusercontent.com/samberubz/mtl.slopes/main/Captur
 # Display the image in Streamlit
 st.image(image1_url, use_column_width=True)
 
-st.write('**Hey! This is an app designed for all of you advanced (and not so advanced) skiers! /n'
+st.write('**Hey! This is an app designed for all of you advanced (and not so advanced) skiers! \n'
          'It currently gathers ski conditions from three stations located within a 2-hour drive from Montreal.'
          'Look, I summarized the main points for you...**')
 st.write('©2024, Samuel Bérubé, P.Eng., M.A.Sc.')
