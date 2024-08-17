97b66563563410265b8051a2da602d5f398b3f5a
samberubz <samuel.berube.1@ens.etsmtl.ca>
2024-08-17 11:47:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
 st.write('test ok')
-st.image('https://github.com/samberubz/mtl.slopes/blob/main/Capture1.png?raw=true')
+# URL of the image from your GitHub repository
+image_url = 'https://github.com/samberubz/mtl.slopes/blob/main/Capture1.png?raw=true'
+
+# Display the image in Streamlit
+st.image(image_url, caption='My Image from GitHub', use_column_width=True)
 
