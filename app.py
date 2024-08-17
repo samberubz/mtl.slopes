2216044bd864619e32aae7ecbdf2cc82afacc417
samberubz <samuel.berube.1@ens.etsmtl.ca>
2024-08-17 12:07:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,7 @@
 import streamlit as st
-import streamlit.components.v1 as components
 
 # Set the page configuration to wide mode
 st.set_page_config(layout="wide")
-
-# Define the HTML content as a string
-html_content = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>My App</title>
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            overflow: hidden;
-        }
-        img {
-            max-width: 100%;
-            height: auto;
-        }
-    </style>
-</head>
-<body>
-    <img src="https://raw.githubusercontent.com/samberubz/mtl.slopes/main/Capture1.png" alt="Image">
-</body>
-</html>
-"""
-
-# Display the HTML content in Streamlit
-components.html(html_content, height=600)
 
 # URL of the image from your GitHub repository
 image1_url = 'https://raw.githubusercontent.com/samberubz/mtl.slopes/main/Capture1.png'
