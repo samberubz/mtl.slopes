cb0e80bcb63099e6199a7199911099f728844680
samberubz <samuel.berube.1@ens.etsmtl.ca>
2024-08-17 12:02:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ import streamlit as st
 # Set the page configuration to wide mode
 st.set_page_config(layout="wide")
 # URL of the image from your GitHub repository
-image_url = 'https://raw.githubusercontent.com/samberubz/mtl.slopes/main/Capture1.png'
+image1_url = 'https://raw.githubusercontent.com/samberubz/mtl.slopes/main/Capture1.png'
+image2_url = 'https://raw.githubusercontent.com/samberubz/mtl.slopes/main/Capture2.png'
 # Display the image in Streamlit
-st.image(image_url, use_column_width=True)
+st.image(image1_url, use_column_width=True)
 
 st.write('**Hey!**')
-st.write('© S. Bérubé, 2024')
+st.write('©2024, Samuel Bérubé, P.Eng., M.A.Sc.')
+
+# Display the image in Streamlit
+st.image(image2_url, use_column_width=True)
 
