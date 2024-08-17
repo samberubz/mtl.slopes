17b15a141b4dce6b599a5e4bc300f56d59efee45
samberubz <samuel.berube.1@ens.etsmtl.ca>
2024-08-17 12:00:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ st.set_page_config(layout="wide")
 # URL of the image from your GitHub repository
 image_url = 'https://raw.githubusercontent.com/samberubz/mtl.slopes/main/Capture1.png'
 # Display the image in Streamlit
-st.image(image_url, caption='© S. Bérubé, 2024', use_column_width=True)
+st.image(image_url, use_column_width=True)
 
 st.write('**Hey!**')
+st.write('© S. Bérubé, 2024')
 
