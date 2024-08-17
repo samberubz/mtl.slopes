5000804d215fef6829741517315d80f7902b18bc
samberubz <samuel.berube.1@ens.etsmtl.ca>
2024-08-17 12:24:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ image2_url = 'https://raw.githubusercontent.com/samberubz/mtl.slopes/main/Captur
 st.image(image1_url, use_column_width=True)
 
 st.write('**Hey! This is an app designed for all of you advanced (and not so advanced) skiers! :skier:**\n\n'
-         '_That includes casual gliders, bunny hill specialists, downhill experts, etc.:snowman:\n\n'
+         '_That includes casual gliders, bunny hill specialists, downhill experts, etc._ :snowman:\n\n'
          'It gathers ski conditions from three well-known stations located within a 2-hour drive from Montreal.:car: \n\n'
          'Look, I summarized the main points for you below. :snow_capped_mountain:')
 st.write('©2024, Samuel Bérubé, P.Eng., M.A.Sc.')
