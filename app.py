f7bc616ad704aa8bf831d6ad0cfa0f6136b93a8f
samberubz <samuel.berube.1@ens.etsmtl.ca>
2024-08-17 11:43:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.write('test ok')
-st.image('Capture1.png')
+st.image('https://github.com/samberubz/mtl.slopes/blob/c2b9be6122e0fbde27bd89141c59899d1fa23ed6/Capture1.png')
 
