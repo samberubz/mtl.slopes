e9a5ad1f93c2dd277b4d8e42da35b442e9b97c35
samberubz <samuel.berube.1@ens.etsmtl.ca>
2024-08-17 11:59:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import streamlit as st
 
-# URL of the image from your GitHub repository
-image_url = 'https://raw.githubusercontent.com/samberubz/mtl.slopes/main/Capture1.png'
-# Display the image in Streamlit
-st.image(image_url, caption='© S. Bérubé, 2024', use_column_width=True)
+st.write('## Responsive Layout Example')
 
+# Create two columns
+col1, col2 = st.columns([1, 2])
+
+with col1:
+    st.image('https://raw.githubusercontent.com/samberubz/mtl.slopes/main/Capture1.png', caption='Image in Column 1', use_column_width=True)
+
+with col2:
+    st.write("This is some text next to the image. It will automatically adjust based on the screen size.")
+
+st.write('## End of Example')
 st.write('**Hey!**')
 
