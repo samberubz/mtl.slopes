1b2bdd31b684fd30794809c57833221ef3005b72
samberubz <samuel.berube.1@ens.etsmtl.ca>
2024-08-17 17:13:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-st.write('This app gathers ski conditions from three well-known stations located within a 2-hour drive from Montreal Montreal'
+st.write('This app gathers ski conditions from three well-known stations located within a 2-hour drive from Montreal \n'
          '(making it your perfect single-stop destination before hitting the slopes). 🚗 \n\n'
          'Look, I summarized the main points for you below. 🏔️')
 st.write('©2024, Samuel Bérubé, P.Eng., M.A.Sc.')
